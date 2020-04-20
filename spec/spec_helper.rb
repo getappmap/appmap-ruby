c8aab2b7f5dbfbf6787ce19c796592bd3b847337
Kevin Gilpin <kgilpin@gmail.com>
2020-04-20 16:16:08 -0400

    Enable rspec --only-failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,7 @@ require 'yaml'
 require 'English'
 require 'webdrivers/chromedriver'
 require 'appmap'
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = "tmp/rspec_failed_examples.txt"
+end
