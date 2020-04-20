6598b8fcf0a2e5f9853f69cfa7d75c9122187509
Kevin Gilpin <kgilpin@gmail.com>
2020-04-20 16:16:38 -0400

    Ensure that caught and recorded exception is re-raised
    In a little bit more idiomatic way

--- lib/appmap/hook.rb
+++ lib/appmap/hook.rb
@@ -133,11 +133,11 @@ module AppMap
                   return_value = base_method.call(*args, &block)
                 rescue
                   exception = $ERROR_INFO
+                  raise
                 ensure
                   with_disabled_hook.call do
                     after_hook.call(call_event, defined_class, method, start_time, return_value, exception)
                   end
-                  raise exception if exception
                 end
               end
             end
