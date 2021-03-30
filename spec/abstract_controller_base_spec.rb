12c4e2de5b32af253d877ba1a2973230f4e31103
Kevin Gilpin <kgilpin@gmail.com>
2021-03-30 17:53:30 -0400

    Run containerized specs once, then assert all the results
    Fill out specs for new functionality:
    Object keys and types
    HTTP headers

--- spec/abstract_controller_base_spec.rb
+++ spec/abstract_controller_base_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_spec_helper'
 
-describe 'AbstractControllerBase' do
+describe 'Rails' do
   %w[5 6].each do |rails_major_version| # rubocop:disable Metrics/BlockLength
-    context "in Rails #{rails_major_version}" do
-      include_context 'Rails app pg database', "spec/fixtures/rails#{rails_major_version}_users_app"
+    context "#{rails_major_version}" do
+      include_context 'Rails app pg database', "spec/fixtures/rails#{rails_major_version}_users_app" unless use_existing_data?
+
       def run_spec(spec_name)
-        FileUtils.rm_rf tmpdir
-        FileUtils.mkdir_p tmpdir
         cmd = <<~CMD.gsub "\n", ' '
           docker-compose run --rm -e RAILS_ENV=test -e APPMAP=true
           -v #{File.absolute_path tmpdir}:/app/tmp app ./bin/rspec #{spec_name}
@@ -18,12 +17,20 @@ describe 'AbstractControllerBase' do
         'tmp/spec/AbstractControllerBase'
       end
 
+      unless use_existing_data?
+        before(:all) do
+          FileUtils.rm_rf tmpdir
+          FileUtils.mkdir_p tmpdir
+          run_spec 'spec/controllers/users_controller_spec.rb'
+          run_spec 'spec/controllers/users_controller_api_spec.rb'
+        end
+      end
+
       let(:appmap) { JSON.parse File.read File.join tmpdir, 'appmap/rspec', appmap_json_file }
       let(:events) { appmap['events'] }
 
-      describe 'testing with rspec' do
-        describe 'creating a user' do
-          before(:all) { run_spec 'spec/controllers/users_controller_api_spec.rb:8' }
+      describe 'an API route' do
+        describe 'creating an object' do
           let(:appmap_json_file) do
             'Api_UsersController_POST_api_users_with_required_parameters_creates_a_user.appmap.json'
           end
@@ -32,7 +39,7 @@ describe 'AbstractControllerBase' do
             expect(File).to exist(File.join(tmpdir, 'appmap/rspec/Inventory.appmap.json'))
           end
 
-          it 'message fields are recorded in the appmap' do
+          it 'http_server_request is recorded in the appmap' do
             expect(events).to include(
               hash_including(
                 'http_server_request' => hash_including(
@@ -54,19 +61,19 @@ describe 'AbstractControllerBase' do
                     'object_id' => Integer
                   )
                 )
-              ),
-              hash_including(
-                'http_server_response' => {
-                  'status' => 201,
-                  'mime_type' => 'application/json; charset=utf-8'
-                }
               )
             )
           end
 
-          context 'with an object-style message' do
-            # TODO
-            it 'message properties are recorded in the appmap'
+          it 'http_server_response is recorded in the appmap' do
+            expect(events).to include(
+              hash_including(
+                'http_server_response' => hash_including(
+                  'status' => 201,
+                  'mime_type' => 'application/json; charset=utf-8'
+                )
+              )
+            )
           end
 
           it 'properly captures method parameters in the appmap' do
@@ -99,10 +106,47 @@ describe 'AbstractControllerBase' do
               'elapsed' => Numeric
             )
           end
+
+          context 'with an object-style message' do
+            let(:appmap_json_file) { 'Api_UsersController_POST_api_users_with_required_parameters_with_object-style_parameters_creates_a_user.appmap.json' }
+
+            it 'message properties are recorded in the appmap' do
+              expect(events).to include(
+                hash_including(
+                  'message' => include(
+                    hash_including(
+                      'name' => 'user',
+                      'properties' => [
+                        { 'name' => 'login', 'class' => 'String' },
+                        { 'name' => 'password', 'class' => 'String' }
+                      ]
+                    )
+                  )
+                )
+              )    
+            end
+          end
         end
 
-        describe 'showing a user' do
-          before(:all) { run_spec 'spec/controllers/users_controller_spec.rb:22' }
+        describe 'listing objects' do
+          context 'with a custom header' do
+            let(:appmap_json_file) { 'Api_UsersController_GET_api_users_with_a_custom_header_lists_the_users.appmap.json' }
+
+            it 'custom header is recorded in the appmap' do
+              expect(events).to include(
+                hash_including(
+                  'http_server_request' => hash_including(
+                    'headers' => hash_including('X-Sandwich' => 'turkey')
+                  )
+                )
+              )
+            end
+          end
+        end
+      end
+
+      describe 'a UI route' do
+        describe 'rendering a page' do
           let(:appmap_json_file) do
             'UsersController_GET_users_login_shows_the_user.appmap.json'
           end
@@ -118,11 +162,6 @@ describe 'AbstractControllerBase' do
               )
             )
           end
-        end
-
-        describe 'listing users' do
-          before(:all) { run_spec 'spec/controllers/users_controller_spec.rb:11' }
-          let(:appmap_json_file) { 'UsersController_GET_users_lists_the_users.appmap.json' }
 
           it 'records and labels view rendering' do
             expect(events).to include hash_including(
@@ -134,7 +173,7 @@ describe 'AbstractControllerBase' do
               'lineno' => Integer,
               'static' => false
             )
-
+  
             expect(appmap['classMap']).to include hash_including(
               'name' => 'action_view',
               'children' => include(hash_including(
@@ -148,7 +187,7 @@ describe 'AbstractControllerBase' do
                 ))
               ))
             )
-          end
+          end  
         end
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,7 @@ require 'appmap'
 RSpec.configure do |config|
   config.example_status_persistence_file_path = "tmp/rspec_failed_examples.txt"
 end
+
+def use_existing_data?
+  ENV['USE_EXISTING_DATA'] == 'true'
+end
