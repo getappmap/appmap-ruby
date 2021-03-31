2808f4c1c7b34956518c2ab34f08e667dbb621a5
Kevin Gilpin <kgilpin@gmail.com>
2021-03-31 17:01:15 -0400

    Simplify header processing; thanks to a better understanding of how Rack works

--- lib/appmap/rails/request_handler.rb
+++ lib/appmap/rails/request_handler.rb
@@ -6,7 +6,6 @@ require 'appmap/hook'
 module AppMap
   module Rails
     module RequestHandler
-<<<<<<< HEAD
       # Host and User-Agent will just introduce needless variation.
       # Content-Type and Authorization get their own fields in the request.
       IGNORE_HEADERS = %w[host user_agent content_type authorization].map(&:upcase).map {|h| "HTTP_#{h}"}.freeze
@@ -23,43 +22,6 @@ module AppMap
               value = kv[1]
               memo[key] = value
             end
-=======
-      MATCH_HEADERS = [
-        %r{^access_control_},
-        %r{^cache_control_},
-        %r{^cross_origin_},
-        %r{^cookie$},
-        %r{^digest$},
-        %r{^etag$},
-        %r{^expect$},
-        %r{^expires$},
-        %r{^if_},
-        %r{^last_modified$},
-        %r{^location$},
-        %r{^set_cookie$},
-        %r{^origin$},
-        %r{^referer$},
-        %r{^referrer_$},
-        %r{^sec_$},
-        %r{^upgrade$},
-        %r{^vary$},
-        %r{^via$},
-        %r{^want_digest$},
-        %r{^www_authenticate$},
-        %r{^x_},
-      ]
-
-      class << self
-        def selected_headers(headers)
-          keep = lambda do |header|
-            header = header.downcase.gsub('-', '_')
-            MATCH_HEADERS.find { |pattern| pattern.match(header) }
-          end
-          matching_headers = headers.each_with_object({}) do |kv, memo|
-            header, value = kv
-            memo[header] = AppMap::Event::MethodEvent.display_string(value) if keep.(header)
-          end
->>>>>>> 34bc65d... Include authorization and selected headers
           matching_headers.blank? ? nil : matching_headers
         end
       end
