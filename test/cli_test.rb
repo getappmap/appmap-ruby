edd56d10d868622351da45ff96c1a55f8f37503d
Alex Torchenko <torchenko@gmail.com>
2021-06-22 12:24:00 -0400

    Process comments

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -12,14 +12,13 @@ packages:
 )
 
   def test_init_when_config_exists
-    output = `./exe/appmap init`
-    p output
+    output = `./exe/appmap-agent-setup init`
     assert_equal 0, $CHILD_STATUS.exitstatus
     assert_includes output, 'The AppMap config file appmap.yml already exists.'
   end
 
   def test_init_with_custom_config_filename
-    output = `./exe/appmap -c #{CONFIG_FILENAME} init`
+    output = `./exe/appmap-agent-setup -c #{CONFIG_FILENAME} init`
     assert_equal 0, $CHILD_STATUS.exitstatus
     assert_includes output, "The following AppMap config file #{CONFIG_FILENAME} has been created:"
     assert_equal EXPECTED_CONFIG_CONTENT, File.read(CONFIG_FILENAME)
@@ -28,7 +27,7 @@ packages:
   end
 
   def test_init_with_custom_config_file_in_subfolder
-    output = `./exe/appmap -c #{SUBFOLDER_CONFIG_FILEPATH} init`
+    output = `./exe/appmap-agent-setup -c #{SUBFOLDER_CONFIG_FILEPATH} init`
     assert_equal 0, $CHILD_STATUS.exitstatus
     assert_includes output, "The following AppMap config file #{SUBFOLDER_CONFIG_FILEPATH} has been created:"
     assert_equal EXPECTED_CONFIG_CONTENT, File.read(SUBFOLDER_CONFIG_FILEPATH)
