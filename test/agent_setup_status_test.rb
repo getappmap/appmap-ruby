1b432039040277e1b5349cc2f75aa436238ea873
Alex Torchenko <torchenko@gmail.com>
2021-07-06 18:13:35 -0400

    feat: define commands as objects

--- test/agent_setup_status_test.rb
+++ test/agent_setup_status_test.rb
@@ -27,39 +27,55 @@ class AgentSetupInitTest < Minitest::Test
   end
 
   def test_status_rails_app
-    def test_status
-      output = `cd spec/fixtures/rails6_users_app && bundle exec ../../../exe/appmap-agent-status`
-      assert_equal 0, $CHILD_STATUS.exitstatus
-      expected = {
-        test_commands: [
-          {
-            framework: :rspec,
-            command: 'APPMAP=true bundle exec rspec spec/controllers'
-          },
-          {
-            framework: :minitest,
-            command: 'APPMAP=true bundle exec ruby test/controllers && APPMAP=true bundle exec ruby test/integration'
-          },
-          {
-            framework: :cucumber,
-            command: 'APPMAP=true bundle exec cucumber'
+    output = `cd spec/fixtures/rails6_users_app && bundle exec ../../../exe/appmap-agent-status`
+    assert_equal 0, $CHILD_STATUS.exitstatus
+    expected = {
+      test_commands: [
+        {
+          framework: :rspec,
+          command: {
+            program: 'bundle',
+            args: %w[exec rspec ./spec/controllers],
+            environment: {
+              APPMAP: 'true'
+            }
           }
-        ],
-        properties: {
-          config: {
-            app: nil,
-            present: true,
-            valid: true
-          },
-          project: {
-            agentVersion: AppMap::VERSION,
-            language: 'ruby',
-            remoteRecordingCapable: false,
-            integrationTests: true
+        },
+        {
+          framework: :minitest,
+          command: {
+            program: 'bundle',
+            args: %w[exec ruby ./test/controllers],
+            environment: {
+              APPMAP: 'true'
+            }
+          }
+        },
+        {
+          framework: :minitest,
+          command: {
+            program: 'bundle',
+            args: %w[exec ruby ./test/integration],
+            environment: {
+              APPMAP: 'true'
+            }
           }
         }
+      ],
+      properties: {
+        config: {
+          app: nil,
+          present: true,
+          valid: false
+        },
+        project: {
+          agentVersion: AppMap::VERSION,
+          language: 'ruby',
+          remoteRecordingCapable: false,
+          integrationTests: true
+        }
       }
-      assert_equal JSON.pretty_generate(expected), output.strip
-    end
+    }
+    assert_equal JSON.pretty_generate(expected), output.strip
   end
 end
