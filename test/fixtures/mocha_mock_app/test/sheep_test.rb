890ea0a8316525a0fd2e1c524a4190c9dac96b0c
Kevin Gilpin <kgilpin@gmail.com>
2021-10-19 14:38:32 -0400

    test: Add test for Mocha::Mock compatibility

---
+++ test/fixtures/mocha_mock_app/test/sheep_test.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+require 'appmap/minitest'
+
+require 'sheep'
+require 'minitest/autorun'
+require 'mocha/minitest'
+
+class SheepTest < Minitest::Test
+  def test_sheep
+    sheep = mock('sheep')
+    sheep.responds_like(Sheep.new)
+    sheep.expects(:baa).returns('baa')
+    sheep.baa
+  end
+end
