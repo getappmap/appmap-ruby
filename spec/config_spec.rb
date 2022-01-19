318d294c1d921dacb18f1d3e1776282d9f208215
Kevin Gilpin <kgilpin@gmail.com>
2022-01-19 14:49:37 -0500

    feat: Label JSON, Marshal and YAML with (de)serialize
    Apply labels serialize, deserialize, and deserialize.unsafe
    to JSON, Marshal, and YAML
    
    Of the three, the safe deserialize are JSON.parse and Psych.safe_load

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -20,235 +20,100 @@ describe AppMap::Config, docker: false do
     }.deep_stringify_keys!
     config = AppMap::Config.load(config_data)
 
-    expect(JSON.parse(JSON.generate(config.as_json))).to eq(JSON.parse(<<~FIXTURE))
-    {
-      "name": "test",
-      "appmap_dir": "tmp/appmap",
-      "packages": [
+    expect(config.as_json.keys.sort).to eq(["appmap_dir", "builtin_hooks", "depends_config", "exclude", "functions", "gem_hooks", "hook_paths", "name", "packages", "swagger_config"])
+    expect(config.as_json['appmap_dir']).to eq('tmp/appmap')
+    expect(config.as_json['name']).to eq('test')
+    expect(config.as_json['packages']).to eq([])
+    expect(config.as_json['depends_config']).to eq({
+      "base_dir" => nil,
+      "base_branches" => [
+        "remotes/origin/main",
+        "remotes/origin/master"
       ],
-      "swagger_config": {
-        "project_name": null,
-        "project_version": "1.0",
-        "output_dir": "swagger",
-        "description": "Generate Swagger from AppMaps"
-      },
-      "depends_config": {
-        "base_dir": null,
-        "base_branches": [
-          "remotes/origin/main",
-          "remotes/origin/master"
-        ],
-        "test_file_patterns": [
-          "spec/**/*_spec.rb",
-          "test/**/*_test.rb"
-        ],
-        "dependent_tasks": [
-          "swagger"
-        ],
-        "description": "Bring AppMaps up to date with local file modifications, and updated derived data such as Swagger files",
-        "rspec_environment_method": "AppMap::Depends.test_env",
-        "minitest_environment_method": "AppMap::Depends.test_env",
-        "rspec_select_tests_method": "AppMap::Depends.select_rspec_tests",
-        "minitest_select_tests_method": "AppMap::Depends.select_minitest_tests",
-        "rspec_test_command_method": "AppMap::Depends.rspec_test_command",
-        "minitest_test_command_method": "AppMap::Depends.minitest_test_command"
-      },
-      "hook_paths": [
-        "pkg",
-        "#{Gem.loaded_specs['activesupport'].gem_dir}"
+      "test_file_patterns" => [
+        "spec/**/*_spec.rb",
+        "test/**/*_test.rb"
       ],
-      "exclude": [
+      "dependent_tasks" => [
+        "swagger"
       ],
-      "functions": [
-        {
-          "cls": "cls",
-          "target_methods": {
-            "package": "pkg",
-            "method_names": [
-              "fn"
-            ]
-          }
-        },
-        {
-          "cls": "cls",
-          "target_methods": {
-            "package": "pkg",
-            "method_names": [
-              "new_fn"
-            ]
-          }
+      "description" => "Bring AppMaps up to date with local file modifications, and updated derived data such as Swagger files",
+      "rspec_environment_method" => "AppMap::Depends.test_env",
+      "minitest_environment_method" => "AppMap::Depends.test_env",
+      "rspec_select_tests_method" => "AppMap::Depends.select_rspec_tests",
+      "minitest_select_tests_method" => "AppMap::Depends.select_minitest_tests",
+      "rspec_test_command_method" => "AppMap::Depends.rspec_test_command",
+      "minitest_test_command_method" => "AppMap::Depends.minitest_test_command"
+    })
+    expect(config.as_json['swagger_config']).to eq({
+      "project_name" => nil,
+      "project_version" => "1.0",
+      "output_dir" => "swagger",
+      "description" => "Generate Swagger from AppMaps"
+    })
+    expect(config.as_json['hook_paths']).to eq([
+      "pkg",
+      "#{Gem.loaded_specs['activesupport'].gem_dir}"
+    ])
+    expect(config.as_json['exclude']).to eq([])
+    expect(config.as_json['functions'].map(&:deep_stringify_keys)).to eq([
+      {
+        "cls" => "cls",
+        "target_methods" => {
+          "package" => "pkg",
+          "method_names" => [
+            :fn
+          ]
         }
-      ],
-      "builtin_hooks": {
-        "JSON::Ext::Parser": [
-          {
-            "package": "json",
-            "method_names": [
-              "parse"
-            ]
-          }
-        ],
-        "JSON::Ext::Generator::State": [
-          {
-            "package": "json",
-            "method_names": [
-              "generate"
-            ]
-          }
-        ],
-        "Logger::LogDevice": [
-          {
-            "package": "logger",
-            "method_names": [
-              "write"
-            ]
-          }
-        ], 
-        "Net::HTTP": [
-          {
-            "package": "net/http",
-            "method_names": [
-              "request"
-            ]
-          }
-        ],
-        "OpenSSL::PKey::PKey": [
-          {
-            "package": "openssl",
-            "method_names": [
-              "sign"
-            ]
-          }
-        ],
-        "OpenSSL::X509::Request": [
-          {
-            "package": "openssl",
-            "method_names": [
-              "sign"
-            ]
-          },
-          {
-            "package": "openssl",
-            "method_names": [
-              "verify"
-            ]
-          }
-        ],
-        "OpenSSL::X509::Certificate": [
-          {
-            "package": "openssl",
-            "method_names": [
-              "sign"
-            ]
-          }
-        ],
-        "OpenSSL::PKCS5": [
-          {
-            "package": "openssl",
-            "method_names": [
-              "pbkdf2_hmac"
-            ]
-          },
-          {
-            "package": "openssl",
-            "method_names": [
-              "pbkdf2_hmac_sha1"
-            ]
-          }
-        ],
-        "OpenSSL::Cipher": [
-          {
-            "package": "openssl",
-            "method_names": [
-              "encrypt"
-            ]
-          },
-          {
-            "package": "openssl",
-            "method_names": [
-              "decrypt"
-            ]
-          }
-        ],
-        "Psych": [
-          {
-            "package": "yaml",
-            "method_names": [
-              "load"
-            ]
-          },
-          {
-            "package": "yaml",
-            "method_names": [
-              "load_stream"
-            ]
-          },
-          {
-            "package": "yaml",
-            "method_names": [
-              "parse"
-            ]
-          },
-          {
-            "package": "yaml",
-            "method_names": [
-              "parse_stream"
-            ]
-          },
-          {
-            "package": "yaml",
-            "method_names": [
-              "dump"
-            ]
-          },
-          {
-            "package": "yaml",
-            "method_names": [
-              "dump_stream"
-            ]
-          }
-        ]
       },
-      "gem_hooks": {
-        "cls": [
-          {
-            "package": "pkg",
-            "method_names": [
-              "fn"
-            ]
-          },
-          {
-            "package": "pkg",
-            "method_names": [
-              "new_fn"
-            ]
-          }
-        ],
-        "ActiveSupport::Callbacks::CallbackSequence": [
-          {
-            "package": "activesupport",
-            "method_names": [
-              "invoke_before"
-            ]
-          },
-          {
-            "package": "activesupport",
-            "method_names": [
-              "invoke_after"
-            ]
-          }
-        ],
-        "ActiveSupport::SecurityUtils": [
-          {
-            "package": "activesupport",
-            "method_names": [
-              "secure_compare"
-            ]
-          }
+      {
+        "cls" => "cls",
+        "target_methods" => {
+          "package" => "pkg",
+          "method_names" => [
+            :new_fn
+          ]
+        }
+      }
+    ])
+    expect(config.as_json['builtin_hooks']).to have_key('JSON::Ext::Parser')
+    expect(config.as_json['builtin_hooks']['JSON::Ext::Parser'].map(&:deep_stringify_keys)).to eq([{
+        "package" => "json",
+        "method_names" => [
+          :parse
         ]
       }
-    }
-    FIXTURE
+    ])
+    expect(config.as_json['gem_hooks']).to have_key('cls')
+    expect(config.as_json['gem_hooks']['cls'].map(&:deep_stringify_keys)).to eq([
+      {
+        "package" => "pkg",
+        "method_names" => [
+          :fn
+        ]
+      },
+      {
+        "package" => "pkg",
+        "method_names" => [
+          :new_fn
+        ]
+      }
+    ])
+    expect(config.as_json['gem_hooks']).to have_key('ActiveSupport::Callbacks::CallbackSequence')
+    expect(config.as_json['gem_hooks']['ActiveSupport::Callbacks::CallbackSequence'].map(&:deep_stringify_keys)).to eq([
+      {
+        "package" => "activesupport",
+        "method_names" => [
+          :invoke_before
+        ]
+      },
+      {
+        "package" => "activesupport",
+        "method_names" => [
+          :invoke_after
+        ]
+      }
+    ])
   end
 
   describe AppMap::Config::Package do
