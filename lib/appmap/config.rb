4cc0e7003a8c37d3b6c8c8bbc68cffac0335b878
Rafał Rzepecki <rafal@app.land>
2022-03-18 15:55:38 -0400

    feat: Autoload hook handlers
    This makes it easier for the user to specify custom handlers.
    For example, specifying
    
        - methods:
          - Acme::Request.make
          handler_class: Acme::Request::AppmapHandler
    
    in the config file will make AppMap automatically require
    `acme/request/appmap_handler` before setting up the hook.

--- lib/appmap/config.rb
+++ lib/appmap/config.rb
@@ -4,8 +4,7 @@ require 'pathname'
 require 'set'
 require 'yaml'
 require 'appmap/util'
-require 'appmap/handler/net_http'
-require 'appmap/handler/rails/template'
+require 'appmap/handler'
 require 'appmap/service/guesser'
 require 'appmap/swagger/configuration'
 require 'appmap/depends/configuration'
@@ -206,7 +205,7 @@ module AppMap
         }.compact
 
         handler_class = hook_decl['handler_class']
-        options[:handler_class] = Object.const_get(handler_class) if handler_class
+        options[:handler_class] = Handler.find(handler_class) if handler_class
 
         package_hooks(methods, **options)
       end
---
+++ lib/appmap/handler.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'active_support/inflector/methods'
+
+module AppMap
+  # Specific hook handler classes and general related utilities.
+  module Handler
+    # Try to find handler module with a given name.
+    #
+    # If the module is not loaded, tries to require the appropriate file
+    # using the usual conventions, eg. `Acme::Handler::AppMap` will try
+    # to require `acme/handler/app_map`, then `acme/handler` and
+    # finally `acme`. Raises NameError if the module could not be loaded
+    # this way.
+    def self.find(name)
+      begin
+        return Object.const_get name
+      rescue NameError
+        try_load ActiveSupport::Inflector.underscore name
+      end
+      Object.const_get name
+    end
+
+    def self.try_load(fname)
+      fname = fname.sub %r{^app_map/}, 'appmap/'
+      fname = fname.split '/'
+      until fname.empty?
+        begin
+          require fname.join '/'
+          return
+        rescue LoadError
+          # pass
+        end
+        fname.pop
+      end
+    end
+  end
+end
