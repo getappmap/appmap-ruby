453f6dfc5de29303fc9cbcf60ce0c3499528711c
Alan Potter <alanjpotter@gmail.com>
2022-03-23 17:00:16 -0400

    fix: Add 3.1 as a supported version

--- lib/appmap/version.rb
+++ lib/appmap/version.rb
@@ -7,7 +7,7 @@ module AppMap
 
   APPMAP_FORMAT_VERSION = '1.5.1'
 
-  SUPPORTED_RUBY_VERSIONS = %w[2.6 2.7 3.0].freeze
+  SUPPORTED_RUBY_VERSIONS = %w[2.6 2.7 3.0 3.1].freeze
 
   DEFAULT_APPMAP_DIR = 'tmp/appmap'.freeze
   DEFAULT_CONFIG_FILE_PATH = 'appmap.yml'.freeze
