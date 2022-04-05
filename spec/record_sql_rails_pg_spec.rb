e4973da6c209e9780c79356770b87c31a1c1cb55
Rafał Rzepecki <rafal@app.land>
2022-04-05 20:11:15 +0200

    test: Refactor sql test for better performance and clarity

--- spec/record_sql_rails_pg_spec.rb
+++ spec/record_sql_rails_pg_spec.rb
@@ -2,91 +2,58 @@ require 'rails_spec_helper'
 
 describe 'SQL events' do
   include_context 'Rails app pg database', 'spec/fixtures/rails6_users_app' do
-    before(:all) { @app.prepare_db }
+    def self.check_queries(cases)
+      cases.each do |test_case, query|
+        context "in #{test_case}" do
+          let(:test_case) { test_case }
+          it "captures #{query}" do
+            expect(sql_events).to include sql_query query
+          end
+        end
+      end
+    end
 
-    before do
-      FileUtils.rm_rf tmpdir
-      app.run_cmd \
-        "./bin/rspec spec/controllers/users_controller_api_spec.rb:#{test_line_number}",
+    context 'with Sequel' do
+      before(:context) { run_specs 'sequel' }
+
+      check_queries(
+        'Api_UsersController_POST_api_users_with_required_parameters_creates_a_user' =>
+          %(INSERT INTO "users" ("login") VALUES ('alice') RETURNING *),
+        'Api_UsersController_GET_api_users_lists_the_users' => %(SELECT * FROM "users")
+      )
+    end
+
+    context 'with ActiveRecord' do
+      before(:context) { run_specs 'activerecord' }
+
+      check_queries(
+        'Api_UsersController_POST_api_users_with_required_parameters_creates_a_user' =>
+          %(INSERT INTO "users" ("login") VALUES ($1) RETURNING "id"),
+        'Api_UsersController_GET_api_users_lists_the_users' => %(SELECT "users".* FROM "users")
+      )
+    end
+
+    def run_specs(orm_module)
+      @app.prepare_db
+      @app.run_cmd \
+        './bin/rspec spec/controllers/users_controller_api_spec.rb:8 spec/controllers/users_controller_api_spec.rb:29',
         'ORM_MODULE' => orm_module,
         'RAILS_ENV' => 'test',
         'APPMAP' => 'true'
     end
 
+    let(:appmap_json) { File.join tmpdir, "appmap/rspec/#{test_case}.appmap.json" }
+    let(:appmap) { JSON.parse(File.read(appmap_json)) }
     let(:tmpdir) { app.tmpdir }
+    let(:sql_events) { appmap['events'].select { |ev| ev.include? 'sql_query' } }
 
-    describe 'fields' do
-      let(:test_line_number) { 8 }
-      let(:appmap_json) { File.join(tmpdir, 'appmap/rspec/Api_UsersController_POST_api_users_with_required_parameters_creates_a_user.appmap.json') }
-      let(:orm_module) { 'sequel' }
-      let(:appmap) { JSON.parse(File.read(appmap_json)) }
-      describe 'on a call event' do
-        let(:event) do
-          appmap['events'].find do |event|
-            event['event'] == 'call' &&
-              event.keys.include?('sql_query')
-          end
-        end
-        it 'do not include function-only fields' do
-          expect(event.keys).to_not include('defined_class')
-          expect(event.keys).to_not include('method_id')
-          expect(event.keys).to_not include('path')
-          expect(event.keys).to_not include('lineno')
-        end
-      end
-    end
-
-    describe 'in a Rails app' do
-      let(:appmap) { JSON.parse(File.read(appmap_json)).to_yaml }
-      context 'while creating a new record' do
-        let(:test_line_number) { 8 }
-        let(:appmap_json) { File.join(tmpdir, 'appmap/rspec/Api_UsersController_POST_api_users_with_required_parameters_creates_a_user.appmap.json') }
-
-        context 'using Sequel ORM' do
-          let(:orm_module) { 'sequel' }
-          it 'detects the sql INSERT' do
-            expect(appmap).to include(<<-SQL_QUERY.strip)
-  sql_query:
-    sql: INSERT INTO "users" ("login") VALUES ('alice') RETURNING *
-            SQL_QUERY
-          end
-        end
-        context 'using ActiveRecord ORM' do
-          let(:orm_module) { 'activerecord' }
-          it 'detects the sql INSERT' do
-            expect(appmap).to include(<<-SQL_QUERY.strip)
-  sql_query:
-    sql: INSERT INTO "users" ("login") VALUES ($1) RETURNING "id"
-            SQL_QUERY
-          end
-        end
-      end
-
-      context 'while listing records' do
-        let(:test_line_number) { 29 }
-        let(:appmap_json) { File.join(tmpdir, 'appmap/rspec/Api_UsersController_GET_api_users_lists_the_users.appmap.json') }
-
-        context 'using Sequel ORM' do
-          let(:orm_module) { 'sequel' }
-          it 'detects the sql SELECT' do
-            expect(appmap).to include(<<-SQL_QUERY.strip)
-  sql_query:
-    sql: SELECT * FROM "users"
-            SQL_QUERY
-
-            expect(appmap).to include('sql:')
-          end
-        end
-        context 'using ActiveRecord ORM' do
-          let(:orm_module) { 'activerecord' }
-          it 'detects the sql SELECT' do
-            expect(appmap).to include(<<-SQL_QUERY.strip)
-  sql_query:
-    sql: SELECT "users".* FROM "users"
-            SQL_QUERY
-          end
-        end
-      end
+    RSpec::Matchers.define_negated_matcher :not_include, :include
+    def sql_query(query)
+      (include('sql_query' => (include 'sql' => query)))
+        .and(not_include('defined_class'))
+        .and(not_include('method_id'))
+        .and(not_include('path'))
+        .and(not_include('lineno'))
     end
   end
 end
