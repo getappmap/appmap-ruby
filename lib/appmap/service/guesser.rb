090f9f39616accd3bc1e9d3cc5f7991232fe5663
Kevin Gilpin <kgilpin@gmail.com>
2022-04-27 16:40:16 -0400

    fix: Guess 'app' as the source path
    appmap-ruby will now expand paths like 'app' to include all sub-packages.
    There's no need to list app/models, app/controllers, etc individually.

--- lib/appmap/service/guesser.rb
+++ lib/appmap/service/guesser.rb
@@ -3,7 +3,7 @@
 module AppMap
   module Service
     class Guesser
-      POSSIBLE_PATHS = %w[app/controllers app/models lib]
+      POSSIBLE_PATHS = %w[app lib]
       class << self
         def guess_name
           return Pathname.new(`git rev-parse --show-toplevel`.strip).basename.to_s if File.directory?('.git')
