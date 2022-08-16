47592fb676c2e7e08e33f5db6446eaf58c253c80
symwell <111290954+symwell@users.noreply.github.com>
2022-08-16 14:47:44 -0400

    Stub out C module for Tracepoint.

---
+++ build_c_tracepoint_module.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# compile and install the C module
+# from: http://www.rubyinside.com/how-to-create-a-ruby-extension-in-c-in-under-5-minutes-100.html
+ruby c_tracepoint_module.rb
+export DESTDIR=`pwd`
+make clean
+make
+make install
+
+# cleanup
+rm -rf Makefile extconf.h usr .sitearchdir.time > /dev/null 2>&1
+rm -f bench_file > /dev/null 2>&1
