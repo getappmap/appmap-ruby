09054f278fee66df2ba913769fa942a3cd30aaec
symwell <111290954+symwell@users.noreply.github.com>
2022-08-18 12:08:38 -0400

    benchmark long string that currently crashes

--- native_c_extension/bench.rb
+++ native_c_extension/bench.rb
@@ -31,6 +31,7 @@ data_str_16  = "1234567890123456"
 data_str_32  = "12345678901234567890123456789012"
 data_str_64  = "1234567890123456789012345678901234567890123456789012345678901234"
 data_str_128 = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678"
+data_str_16376 = "1" * 16376 # stringifying this used to crash
 data_file = File.new("bench_file", "w")
 data_net_http = Net::HTTP.new('example.com')
 data_net_httpgenericrequest = Net::HTTPGenericRequest.new('m', 'request_body', 'response_body', 'example.com')
@@ -98,6 +99,7 @@ symbols = [
   :data_str_32,
   :data_str_64,
   :data_str_128,
+  :data_str_16376,
   # :data_file,
   # :data_net_http,
   # :data_net_httpgenericrequest,
