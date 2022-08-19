ad15fda9e1a57e80e8cb1bb36e8e9edeec37856b
symwell <111290954+symwell@users.noreply.github.com>
2022-08-19 13:00:03 -0400

    when debugging, cp event.rb

--- native_c_extension/build_in_specific_dir.sh
+++ native_c_extension/build_in_specific_dir.sh
@@ -13,8 +13,9 @@
 TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-61177cc3accd/lib/appmap/
 TARGET_DIR=$HOME/devel/sample_app_6th_ed/gems/gems/appmap-0.86.0/lib/appmap/
 TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/gems/appmap-0.86.0/lib/appmap
+#TARGET_DIR=$HOME/devel/sample_app_6th_ed/gems/gems/appmap-0.86.0/lib/appmap
 
-FILES="c_custom_to_s.c c_custom_to_s_module.rb build_c_custom_to_s_module.sh"
+FILES="c_custom_to_s.c c_custom_to_s_module.rb build_c_custom_to_s_module.sh ../lib/appmap/event.rb"
 for file in $FILES; do
     cp $file $TARGET_DIR/.
 done
