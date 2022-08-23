a28ce0e4c224948df911b7f15dbdf04625b6b11d
symwell <111290954+symwell@users.noreply.github.com>
2022-08-23 16:06:45 -0400

    add TARGET_DIRs when debugging solidus, appmap-server

--- native_c_extension/build_in_specific_dir.sh
+++ native_c_extension/build_in_specific_dir.sh
@@ -10,11 +10,10 @@
 #     exit 0
 # fi
 
-TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-61177cc3accd/lib/appmap/
-TARGET_DIR=$HOME/devel/sample_app_6th_ed/gems/gems/appmap-0.86.0/lib/appmap/
-TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/gems/appmap-0.86.0/lib/appmap
-#TARGET_DIR=$HOME/devel/sample_app_6th_ed/gems/gems/appmap-0.86.0/lib/appmap
-TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
+TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-61177cc3accd/lib/appmap
+#TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
+TARGET_DIR=$HOME/devel/solidus/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
+TARGET_DIR=$HOME/devel/appmap-server/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-970d5f8f2f39/lib/appmap
 
 FILES="c_custom_to_s.c c_custom_to_s_module.rb build_c_custom_to_s_module.sh ../lib/appmap/event.rb"
 for file in $FILES; do
