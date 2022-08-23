c30256875e08361a8dcc7d5b73941e530923162d
symwell <111290954+symwell@users.noreply.github.com>
2022-08-23 12:52:15 -0400

    add an implementation of utf8_encode that seems to work; it doesn't support replacing an illegal character

--- native_c_extension/c_custom_to_s.c
+++ native_c_extension/c_custom_to_s.c
@@ -233,6 +233,159 @@ VALUE method_c_custom_to_s_hash(VALUE self, VALUE value) {
   return ret;
 }
 
+/* Returns the bits of an 32-bit unsigned int variable over a given
+ * position. Begin bit is included, but end bit is excluded. */
+static unsigned int get_bits(unsigned int char_reg, int begin, int end)
+{
+    int i = 0;
+    unsigned int bits = 0;
+
+    for (i = begin; i < end; i++)
+    {
+        if (char_reg & (1 << i))
+        {
+            bits |= (1 << (i - begin));
+        }
+    }
+
+    return bits;
+}
+
+/* Counts the number of 8-bit bytes that are required to encode a given
+ * Unicode code point using UTF-8 coding. */
+static int get_num_bytes(unsigned int character)
+{
+    int num_bytes = 0;
+    int i = 0;
+    int bit_pos = 0;
+    const int num_bits = sizeof(unsigned int) * CHAR_BIT;
+
+    for (i = 0; i < num_bits; i++)
+    {
+        if (character & (1 << i))
+        {
+            bit_pos = i;
+        }
+    }
+
+    /* Double-byte encoding (e.g. Scandinavian letters) */
+    if ((bit_pos > 6) && (bit_pos <= 10))
+    {
+        num_bytes = 2;
+    }
+    /* Triple-byte encoding (e.g. CJK characters) */
+    else if ((bit_pos > 10) && (bit_pos <= 15))
+    {
+        num_bytes = 3;
+    }
+    /* Quad-byte encoding (e.g. special characters) */
+    else if (bit_pos > 15)
+    {
+        num_bytes = 4;
+    }
+    /* Single-byte encoding (ASCII characters) */
+    else
+    {
+        num_bytes = 1;
+    }
+
+    return num_bytes;
+}
+
+/*
+ * Encodes the given character's Unicode code point using UTF-8 coding.
+ * Parameters:
+ *      character   Unicode code point as an unsigned integer.
+ *      utf8        Array for UTF-8 encoded bytes (can be 1..4 bytes).
+ */
+int utf8_encode(unsigned int character, unsigned char utf8[])
+{
+    int num_bytes = 0;
+    const int utf8_len = 4+1;
+    int i = 0;
+    unsigned int tmp_bits = 0;
+
+    num_bytes = get_num_bytes(character);
+
+    for (i = 0; i < utf8_len; i++)
+    {
+        utf8[i] = '\0';
+    }
+
+    switch (num_bytes)
+    {
+    case 1:
+        /* utf8[0]: 0xxx|xxxx -> 7 databits (0..127) */
+        tmp_bits = get_bits(character, 0, 8);
+        utf8[0] = (unsigned char)tmp_bits;
+        return 1;
+        break;
+
+    case 2:
+        /* utf8[0]: 110x|xxxx
+           utf8[1]: 10xx|xxxx -> 11 databits (128..2047) */
+        tmp_bits = get_bits(character, 0, 6);
+        utf8[1] = (unsigned char)tmp_bits;
+
+        tmp_bits = get_bits(character, 6, 11);
+        utf8[0] = (unsigned char)tmp_bits;
+
+        utf8[0] |= (1 << 6) | (1 << 7);
+        utf8[1] |= (1 << 7);
+        return 2;
+        break;
+
+    case 3:
+        /* utf8[0]: 1110|xxxx
+           utf8[1]: 10xx|xxxx
+           utf8[2]: 10xx|xxxx -> 16 databits (2048..65 535) */
+        tmp_bits = get_bits(character, 0, 6);
+        utf8[2] = (unsigned char)tmp_bits;
+
+        tmp_bits = get_bits(character, 6, 12);
+        utf8[1] = (unsigned char)tmp_bits;
+
+        tmp_bits = get_bits(character, 12, 16);
+        utf8[0] = (unsigned char)tmp_bits;
+
+        utf8[0] |= (1 << 5) | (1 << 6) | (1 << 7);
+        utf8[1] |= (1 << 7);
+        utf8[2] |= (1 << 7);
+        return 3;
+        break;
+
+    case 4:
+        /* utf8[0]: 1111|0xxx
+           utf8[1]: 10xx|xxxx
+           utf8[2]: 10xx|xxxx
+           utf8[3]: 10xx|xxxx -> 21 databits (65 536..2 097 151) */
+        tmp_bits = get_bits(character, 0, 6);
+        utf8[3] = (unsigned char)tmp_bits;
+
+        tmp_bits = get_bits(character, 6, 12);
+        utf8[2] = (unsigned char)tmp_bits;
+
+        tmp_bits = get_bits(character, 12, 18);
+        utf8[1] = (unsigned char)tmp_bits;
+
+        tmp_bits = get_bits(character, 18, 21);
+        utf8[0] = (unsigned char)tmp_bits;
+
+        utf8[0] |= (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+        utf8[1] |= (1 << 7);
+        utf8[2] |= (1 << 7);
+        utf8[3] |= (1 << 7);
+        return 4;
+        break;
+
+    default:
+        return 0;
+        break;
+    }
+}
+
+
+
 // This is the method we added to test out passing parameters
 // from https://silverhammermba.github.io/emberb/c/
 VALUE method_c_custom_to_s(VALUE self, VALUE first) {
@@ -297,13 +450,22 @@ VALUE method_c_custom_to_s(VALUE self, VALUE first) {
       offset += buffer_small_len;
     }
 
-    // this is 600%-900%x faster but reports the error:
+    // convert to unicode
+    char buffer_utf8_encoded[offset * 4];
+    int counter = 0;
+    while (counter < offset) {
+      unsigned int character = buffer[counter];
+      counter += utf8_encode(character, (unsigned char *) &buffer_utf8_encoded[counter]);
+    }
+    ret = rb_utf8_str_new(buffer_utf8_encoded, counter);
+
+    // this is 600%-900%x faster than calling Ruby but reports the error:
     // in `generate': source sequence is illegal/malformed utf-8 (JSON::GeneratorError)
-    //ret = rb_utf8_str_new(buffer, offset);
+    // ret = rb_utf8_str_new(buffer, offset);
 
     // call Ruby function to utf8 encode instead of encode in C
-    VALUE string_unencoded = rb_str_new_cstr(buffer);
-    ret = rb_funcall(self, rb_intern("custom_display_string_c_encode_utf8"), 1, string_unencoded);
+    /* VALUE string_unencoded = rb_str_new_cstr(buffer); */
+    /* ret = rb_funcall(self, rb_intern("custom_display_string_c_encode_utf8"), 1, string_unencoded); */
     break;
   }
   case T_ARRAY: {
