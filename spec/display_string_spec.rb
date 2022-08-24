f9651eef80d7fab5b3e7865b733bb7b8c0e4ab5f
symwell <111290954+symwell@users.noreply.github.com>
2022-08-24 17:11:31 -0400

    fix Array and Hash tests now that strings are returned quoted. Add test for Array with nil

--- spec/display_string_spec.rb
+++ spec/display_string_spec.rb
@@ -22,7 +22,10 @@ describe 'display_string' do
       compare_display_string 'foo' * 100, 'foo' * 33 + 'f (...200 more characters)'
     end
     it 'Array' do
-      compare_display_string([ 1, 'my', :bar, [ 2, 3 ], { 4 => 5 } ], '[1, my, :bar, [2, 3], {4=>5}]')
+      compare_display_string([ 1, 'my', :bar, [ 2, 3 ], { 4 => 5 } ], '[1, "my", :bar, [2, 3], {4=>5}]')
+    end
+    it 'Array with nil' do
+      compare_display_string([ 1, nil, 3 ], '[1, nil, 3]')
     end
     it 'large Array' do
       compare_display_string 50.times.map { |i| i }, '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9 (...40 more items)]'
@@ -31,7 +34,7 @@ describe 'display_string' do
       compare_display_string(50.times.map { |i| [ i*2, i*2+1] }.to_h, '{0=>1, 2=>3, 4=>5, 6=>7, 8=>9, 10=>11, 12=>13, 14=>15, 16=>17, 18=>19 (...40 more entries)}')
     end
     it 'Hash' do
-      compare_display_string({ 1 => 2, 'my' => 'neighbor', bar: :baz, ary: [ 1, 2 ]}, '{1=>2, my=>neighbor, :bar=>:baz, :ary=>[1, 2]}')
+      compare_display_string({ 1 => 2, 'my' => 'neighbor', bar: :baz, ary: [ 1, 2 ]}, '{1=>2, "my"=>"neighbor", :bar=>:baz, :ary=>[1, 2]}')
     end
     it 'big Hash' do
       compare_display_string(50.times.map { |i| [ i*2, i*2+1] }.to_h, '{0=>1, 2=>3, 4=>5, 6=>7, 8=>9, 10=>11, 12=>13, 14=>15, 16=>17, 18=>19 (...40 more entries)}')
