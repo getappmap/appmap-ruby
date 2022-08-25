29d7e29a67d9add924f6cadedd01c53624cc9270
symwell <111290954+symwell@users.noreply.github.com>
2022-08-25 09:31:10 -0400

    compile c_custom_to_s_test.c without interfering with the C extension

--- ext/ccustomtos/test.sh
+++ ext/ccustomtos/test.sh
@@ -5,4 +5,8 @@
 #gcc c_custom_to_s.c c_custom_to_s_test.c -I/usr/include/x86_64-linux-gnu/ruby-2.7.0 -I/usr/include/ruby-2.7.0 -lruby-2.7 -lm
 # pkg-config --cflags --libs ruby
 
+# don't keep this file name .C else `bundle exec rake compile`
+# attempts to compile it too and it conflicts with the extension
+cp c_custom_to_s_test.c.disabled c_custom_to_s_test.c
 gcc c_custom_to_s_test.c -I/usr/include/x86_64-linux-gnu/ruby-2.7.0 -I/usr/include/ruby-2.7.0 -lruby-2.7 -lm
+rm -f c_custom_to_s_test.c
