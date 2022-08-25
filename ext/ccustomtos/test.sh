83b24012dfc0e9dc21e8ddd02d84344cab6aacc2
symwell <111290954+symwell@users.noreply.github.com>
2022-08-25 09:29:19 -0400

    rename

--- ext/ccustomtos/test.sh
+++ ext/ccustomtos/test.sh
@@ -5,4 +5,4 @@
 #gcc c_custom_to_s.c c_custom_to_s_test.c -I/usr/include/x86_64-linux-gnu/ruby-2.7.0 -I/usr/include/ruby-2.7.0 -lruby-2.7 -lm
 # pkg-config --cflags --libs ruby
 
-gcc c_custom_to_s_test.cpp -I/usr/include/x86_64-linux-gnu/ruby-2.7.0 -I/usr/include/ruby-2.7.0 -lruby-2.7 -lm
+gcc c_custom_to_s_test.c -I/usr/include/x86_64-linux-gnu/ruby-2.7.0 -I/usr/include/ruby-2.7.0 -lruby-2.7 -lm
