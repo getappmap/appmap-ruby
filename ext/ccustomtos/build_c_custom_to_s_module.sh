3d14294c9c00c358506b753445c4aca5fd384e35
symwell <111290954+symwell@users.noreply.github.com>
2022-08-25 09:26:33 -0400

    ruby extconf.rb

--- ext/ccustomtos/build_c_custom_to_s_module.sh
+++ ext/ccustomtos/build_c_custom_to_s_module.sh
@@ -2,7 +2,7 @@
 
 # compile and install the C module
 # from: http://www.rubyinside.com/how-to-create-a-ruby-extension-in-c-in-under-5-minutes-100.html
-ruby c_custom_to_s_module.rb
+ruby extconf.rb
 export DESTDIR=`pwd`
 make clean
 RUBY_VERSION_DIR=`rbenv prefix`
