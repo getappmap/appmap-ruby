38b961806ff792337c1426a67b477d36c28bcc7d
symwell <111290954+symwell@users.noreply.github.com>
2022-08-26 11:53:56 -0400

    copy file extconf.rb; and to different dir

--- ext/ccustomtos/build_in_specific_dir.sh
+++ ext/ccustomtos/build_in_specific_dir.sh
@@ -3,17 +3,18 @@
 # used to debug
 
 
-TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-61177cc3accd/lib/appmap
-#TARGET_DIR=$HOME/devel/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
-TARGET_DIR=$HOME/devel/solidus/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
-TARGET_DIR=$HOME/devel/appmap-server/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-970d5f8f2f39/lib/appmap
+TARGET_DIR=$HOME/src/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-61177cc3accd/lib/appmap
+#TARGET_DIR=$HOME/src/sample_app_6th_ed/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
+TARGET_DIR=$HOME/src/solidus/vendor/bundle/ruby/3.0.0/gems/appmap-0.87.0/lib/appmap
+TARGET_DIR=$HOME/src/appmap-server/vendor/bundle/ruby/3.0.0/bundler/gems/appmap-ruby-970d5f8f2f39/lib/appmap
+TARGET_DIR=$HOME/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/gems/appmap-0.87.0/lib/appmap
 
 TARGET_DIR_INPUT=$1
 if [ "$TARGET_DIR_INPUT" != "" ]; then
     TARGET_DIR=$TARGET_DIR_INPUT
 fi
 
-FILES="c_custom_to_s.c c_custom_to_s_module.rb build_c_custom_to_s_module.sh ../lib/appmap/event.rb"
+FILES="c_custom_to_s.c extconf.rb build_c_custom_to_s_module.sh ../../lib/appmap/event.rb"
 for file in $FILES; do
     cp $file $TARGET_DIR/.
 done
