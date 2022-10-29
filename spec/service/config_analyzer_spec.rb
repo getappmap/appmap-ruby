f660b77d543fc7760a74a2566e2ee92d96fc954d
Rafał Rzepecki <rafal@app.land>
2022-10-29 09:42:12 +0200

    Refactor spec/service/config_analyzer_spec.rb
    DRY up the test cases, removing misleading out-of-date example
    titles, consolidating examples and simplifying code.

--- spec/service/config_analyzer_spec.rb
+++ spec/service/config_analyzer_spec.rb
@@ -7,96 +7,51 @@ describe AppMap::Service::ConfigAnalyzer do
   subject { described_class.new(config_file) }
 
   context 'with non-existing config' do
-    let(:config_file) { 'spec/fixtures/config/non_existing_config.yml'}
+    let(:config_file) { 'spec/fixtures/config/non_existing_config.yml' }
 
-    describe '.app_name' do
-      it 'returns nil' do
-        expect(subject.app_name).to be_nil
-      end
-    end
-
-    describe '.is_valid?' do
-      it 'returns false' do
-        expect(subject.valid?).to be_falsey
-      end
-    end
-
-    describe '.is_present?' do
-      it 'returns false' do
-        expect(subject.present?).to be_falsey
-      end
+    example do
+      expect(subject).to_not be_valid
+      expect(subject.app_name).to be_nil
+      expect(subject).to_not be_present
     end
   end
 
   context 'with valid but non rails config' do
-    let(:config_file) { 'spec/fixtures/config/valid_config.yml'}
+    let(:config_file) { 'spec/fixtures/config/valid_config.yml' }
 
-    describe '.app_name' do
-      it 'returns app name value from config' do
-        expect(subject.app_name).to eq('appmap')
-      end
-    end
-
-    it 'is valid' do
-      expect(subject.errors).to eq(['AppMap auto-configuration is currently not available for non Rails projects'])
-    end
-
-    describe '.is_present?' do
-      it 'returns true' do
-        expect(subject.present?).to be_truthy
-      end
+    example do
+      expect(subject).to be_present
+      expect(subject.app_name).to eq 'appmap'
+      expect(subject.errors).to eq ['AppMap auto-configuration is currently not available for non Rails projects']
+      expect(subject).to_not be_valid
     end
   end
 
   context 'with maximal valid config' do
-    let(:config_file) { 'spec/fixtures/config/maximal_config.yml'}
+    let(:config_file) { 'spec/fixtures/config/maximal_config.yml' }
 
-    it 'is valid' do
+    example do
       expect(subject.errors).to eq(['AppMap auto-configuration is currently not available for non Rails projects'])
     end
   end
 
   context 'with invalid YAML config' do
-    let(:config_file) { 'spec/fixtures/config/invalid_yaml_config.yml'}
+    let(:config_file) { 'spec/fixtures/config/invalid_yaml_config.yml' }
 
-    describe '.app_name' do
-      it 'returns app name value from config' do
-        expect(subject.app_name).to be_nil
-      end
-    end
-
-    describe '.is_valid?' do
-      it 'returns false' do
-        expect(subject.valid?).to be_falsey
-      end
-    end
-
-    describe '.is_present?' do
-      it 'return true' do
-        expect(subject.present?).to be_truthy
-      end
+    example do
+      expect(subject).to be_present
+      expect(subject.app_name).to be_nil
+      expect(subject).to_not be_valid
     end
   end
 
   context 'with incomplete config' do
-    let(:config_file) { 'spec/fixtures/config/incomplete_config.yml'}
+    let(:config_file) { 'spec/fixtures/config/incomplete_config.yml' }
 
-    describe '.app_name' do
-      it 'returns nil' do
-        expect(subject.app_name).to eq('app')
-      end
-    end
-
-    describe '.is_valid?' do
-      it 'guesses paths and returns true ' do
-        expect(subject.valid?).to be_falsey
-      end
-    end
-
-    describe '.is_present?' do
-      it 'returns true' do
-        expect(subject.present?).to be_truthy
-      end
+    example do
+      expect(subject).to be_present
+      expect(subject.app_name).to eq('app')
+      expect(subject).to_not be_valid
     end
   end
 end
