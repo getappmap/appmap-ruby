9a2877396187393adfe6e4379f0d6a735d5678b4
Dustin Byrne <dustinsbyrne@gmail.com>
2023-02-02 16:39:14 -0500

    feat: Implement `items` Parameter object format (AppMap v1.10.0)

--- lib/appmap/value_inspector.rb
+++ lib/appmap/value_inspector.rb
@@ -3,6 +3,7 @@ module AppMap
     extend self
 
     MAX_DEPTH = 3
+    MAX_ARRAY_ELEMENTS = 5
 
     def detect_size(value)
       # Don't risk calling #size on things like data-access objects, which can and will issue queries for this information.
@@ -11,10 +12,17 @@ module AppMap
       end
     end
 
-    def detect_schema(value, max_depth: MAX_DEPTH, type_info: {}, observed_values: Set.new(), depth: 0)
-      return type_info if depth == max_depth
+    def detect_schema(
+      value,
+      max_depth: MAX_DEPTH,
+      max_array_elements: MAX_ARRAY_ELEMENTS,
+      type_info: { class: best_class_name(value) },
+      observed_values: Set.new,
+      depth: 0
+    )
+      return type_info if depth >= max_depth && !array_like?(value)
 
-      if value.respond_to?(:keys)
+      if hash_like?(value)
         return if observed_values.include?(value.object_id)
 
         observed_values << value.object_id
@@ -23,17 +31,22 @@ module AppMap
           next_value = value[key]
 
           value_schema = begin
-              { name: key, class: best_class_name(next_value) }
-            rescue
-              warn "Error in add_schema(#{next_value.class})", $!
-              raise
-            end
+            { name: key, class: best_class_name(next_value) }
+          rescue
+            warn "Error in add_schema(#{next_value.class})", $!
+            raise
+          end
 
           detect_schema(next_value, **{ max_depth: max_depth, type_info: value_schema, observed_values: observed_values, depth: depth + 1 })
         end.compact
         type_info[:properties] = properties unless properties.empty?
-      elsif value.respond_to?(:first)
-        detect_schema(value.first, **{ max_depth: max_depth, type_info: type_info, observed_values: observed_values, depth: depth + 1 })
+      elsif array_like?(value)
+        type_info[:items] = value.take(max_array_elements).map do |next_value|
+          value_schema = { class: best_class_name(next_value) }
+          detect_schema(next_value, **{ max_depth: max_depth, type_info: value_schema, observed_values: observed_values, depth: depth + 1 })
+        end
+
+        type_info[:items] = type_info[:items].compact.uniq(&:hash)
       end
       type_info
     end
@@ -45,6 +58,16 @@ module AppMap
         value_cls = value_cls.superclass
       end
       value_cls&.name || "unknown"
+    end
+
+    private
+
+    def array_like?(value)
+      value.is_a?(Enumerable) && !hash_like?(value)
+    end
+
+    def hash_like?(value)
+      value.respond_to?(:keys)
     end
   end
 end
