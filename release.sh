1301566d1c5942d4d211501687f9f333af29eaaa
Rafał Rzepecki <divided.mind@gmail.com>
2026-03-21 22:14:34 +0100

    chore(ci): Migrate from Travis to GH actions

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@
 # using bash wrapper as Rake blows up in `require/extentiontask` (line 10)
 
 RELEASE_FLAGS=""
-if [ ! -z "$TRAVIS_REPO_SLUG" ]; then
-    RELEASE_FLAGS="-r git+https://github.com/${TRAVIS_REPO_SLUG}.git"
-fi 
+if [ ! -z "$GITHUB_REPOSITORY" ]; then
+    RELEASE_FLAGS="-r git+https://github.com/${GITHUB_REPOSITORY}.git"
+fi
 
 if [ ! -z "$GEM_ALTERNATIVE_NAME" ]; then
     echo "Release: GEM_ALTERNATIVE_NAME=$GEM_ALTERNATIVE_NAME"
